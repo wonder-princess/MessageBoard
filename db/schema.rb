0a2942a5866484b790a5d39daa82c63280b3b043
wonder-princess <sekai_love.me.do@hotmail.co.jp>
2021-08-18 22:37:03 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_042650) do
+ActiveRecord::Schema.define(version: 2021_08_18_222717) do
 
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
 end
