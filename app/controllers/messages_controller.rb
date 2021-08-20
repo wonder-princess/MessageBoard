45df704996647592280c99a2aaa266bbac904601
wonder-princess <sekai_love.me.do@hotmail.co.jp>
2021-08-20 15:41:32 +0000

    add bootstrap design

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,8 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
-  　@pagy, @messages = pagy(Message.order(id: :desc), items:3)
+    # @pagy, @messages = pagy(Message.all)
+    @pagy, @messages = pagy(Message.order(id: :desc), items:3)
   end
 
   def show
